cd4072d0914d582a58d1aaf97ce4cf01c3097e68
Angus Bezzina <angus@33bondi.com>
2022-01-11 10:56:14 -0600

    Build: update node, data structures + localization

--- src/api/articulo/content-types/articulo/lifecycles.js
+++ src/api/articulo/content-types/articulo/lifecycles.js
@@ -3,14 +3,14 @@ const slugify = require("slugify");
 module.exports = {
   beforeCreate(event) {
     const { data } = event.params;
-    if (data.title) {
-      data.slug = slugify(data.title, { lower: true, strict: true });
+    if (data.Titulo) {
+      data.Slug = slugify(data.Titulo, { lower: true, strict: true });
     }
   },
   beforeUpdate(event) {
     const { data } = event.params;
-    if (data.title) {
-      data.slug = slugify(data.title, { lower: true, strict: true });
+    if (data.Titulo) {
+      data.Slug = slugify(data.Titulo, { lower: true, strict: true });
     }
   },
 };

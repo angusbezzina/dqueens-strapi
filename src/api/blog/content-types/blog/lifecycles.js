03c236321d1500029f3a7553f47dc5ab03ae321b
Angus Bezzina <angus@33bondi.com>
2022-01-28 20:23:38 -0600

    build: update lifecycle method & content type schema

--- src/api/blog/content-types/blog/lifecycles.js
+++ src/api/blog/content-types/blog/lifecycles.js
@@ -3,14 +3,14 @@ const slugify = require("slugify");
 module.exports = {
   beforeCreate(event) {
     const { data } = event.params;
-    if (data.Titulo) {
-      data.Slug = slugify(data.Titulo, { lower: true, strict: true });
+    if (data.titulo) {
+      data.slug = slugify(data.titulo, { lower: true, strict: true });
     }
   },
   beforeUpdate(event) {
     const { data } = event.params;
-    if (data.Titulo) {
-      data.Slug = slugify(data.Titulo, { lower: true, strict: true });
+    if (data.titulo) {
+      data.slug = slugify(data.titulo, { lower: true, strict: true });
     }
   },
 };
--- src/api/servicio/content-types/servicio/lifecycles.js
+++ src/api/servicio/content-types/servicio/lifecycles.js
@@ -4,13 +4,13 @@ module.exports = {
   beforeCreate(event) {
     const { data } = event.params;
     if (data.titulo) {
-      data.Slug = slugify(data.titulo, { lower: true, strict: true });
+      data.slug = slugify(data.titulo, { lower: true, strict: true });
     }
   },
   beforeUpdate(event) {
     const { data } = event.params;
     if (data.titulo) {
-      data.Slug = slugify(data.titulo, { lower: true, strict: true });
+      data.slug = slugify(data.titulo, { lower: true, strict: true });
     }
   },
 };
